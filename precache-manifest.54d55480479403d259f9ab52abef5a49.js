bd77e82b1ed901f17af15fc94a3f07fcbb5f31aa
Dung <dung.dao19@outlook.com>
2020-08-01 22:41:30 -0500

    Updates

---
+++ precache-manifest.54d55480479403d259f9ab52abef5a49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fef789f9102becc76d0c33b63861aac",
+    "url": "/face_recognition_app/index.html"
+  },
+  {
+    "revision": "e958d1eaba2d179d551e",
+    "url": "/face_recognition_app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "24f448fc3d9350385c04",
+    "url": "/face_recognition_app/static/css/main.fed72c86.chunk.css"
+  },
+  {
+    "revision": "e958d1eaba2d179d551e",
+    "url": "/face_recognition_app/static/js/2.561fa189.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/face_recognition_app/static/js/2.561fa189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f448fc3d9350385c04",
+    "url": "/face_recognition_app/static/js/main.38b12ae3.chunk.js"
+  },
+  {
+    "revision": "5b6a57322c4a169453cf",
+    "url": "/face_recognition_app/static/js/runtime-main.60d5fc75.js"
+  },
+  {
+    "revision": "17bdb77a8d7dd44559050bf821e4cbb3",
+    "url": "/face_recognition_app/static/media/anime.17bdb77a.png"
+  }
+]);
